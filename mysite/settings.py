9a480dc30ba68e4bc51df201f4c7bbcedcd82e3e
htadg <sharmahiten3@gmail.com>
2015-11-18 14:49:56 +0530

    Forms Added

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's_h3n^qrfood942%($=a+97_@&4_rnuog=30!)ixr)pq4i85u7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
