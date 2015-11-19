74335e59c7b3649bc9a7c90107a8d44b6d844789
htadg <sharmahiten3@gmail.com>
2015-11-19 18:51:47 +0530

    Deletion Added

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+	url(r'^deleted/(?P<pk>[0-9]+)/$', views.post_delete, name='post_delete'),
+	url(r'^confirm_deletion/(?P<pk>[0-9]+)/$', views.post_confirm_delete, name='post_confirm_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import redirect
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_confirm_delete(request,pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_confirm_delete.html', {'post':post})
+	
+def post_delete(request,pk):
+	Post.objects.filter(pk=pk).delete() 
+	return render(request, 'blog/post_delete.html')
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -21,11 +29,11 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            post = get_object_or_404(Post, pk=pk)
-            return render(request, 'blog/post_detail.html',{'post': post})
+            return render(request, 'blog/post_detail.html', {'post': post})
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -37,7 +45,7 @@ def post_edit(request, pk):
             post.published_date = timezone.now()
             post.save()
             post = get_object_or_404(Post, pk=pk)
-            return render(request, 'blog/post_detail.html',{'post': post})
+            return render(request, 'blog/post_detail.html', {'post': post})
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
